01141571f406f7b17bc7aaf215bb8c587cb06625
sina-emilian <mail@sinas.net>
2025-09-29 17:13:07 +0200

    Template for main

---
+++ src/lib/Main/Template.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import { connection, config, editMode, templates, lang } from '$lib/Stores';
+	import { marked } from 'marked';
+	import { onDestroy } from 'svelte';
+	import { openModal } from 'svelte-modals';
+	import type { TemplateItem } from '$lib/Types';
+
+	export let sel: TemplateItem;
+	export let demo = false;
+
+	let unsubscribe: () => void;
+	let id = sel?.id;
+	let error = false;
+
+	$: template = sel?.template;
+
+	$: if ($config?.state === 'RUNNING' && template) {
+		renderTemplate(template);
+	}
+	/**
+	 * Renders template by id to `$templates`
+	 */
+	async function renderTemplate(data: string) {
+		if (!$connection) return;
+
+		const handleResponse = (response: { result?: string }) => {
+			if (response?.result && id) {
+				$templates[id] = marked.parse(response.result) as any;
+				error = false;
+			}
+		};
+
+		const handleError = (err: any) => {
+			if (err?.code === 'template_error' && id) {
+				console.warn('render_template', err);
+				$templates[id] = err.message;
+				error = true;
+			}
+		};
+
+		try {
+			unsubscribe = await $connection.subscribeMessage(handleResponse, {
+				type: 'render_template',
+				template: data
+			});
+		} catch (error) {
+			handleError(error);
+		}
+	}
+	/**
+	 * Handle click
+	 */
+	async function handleClick() {
+		if ($editMode) {
+			openModal(() => import('$lib/Modal/TemplateConfig.svelte'), { sel });
+		}
+	}
+</script>
+
+<div
+	id="markdown"
+	on:click={handleClick}
+	on:keydown
+	role="button"
+	tabindex="0"
+>
+	{#if demo}
+		<div class="template">
+			<span>&#123;&#123;</span> template <span>&#125;&#125;</span>
+		</div>
+	{:else if id && $templates?.[id]}
+		<span class:error>{@html $templates?.[id]}</span>
+	{:else if template}
+		{$lang('unknown')}
+	{:else}
+		{$lang('template')}
+	{/if}
+</div>
+
+<style>
+	.container {
+		width: 14.5rem;
+		background-color: rgba(255, 255, 255, 0.25);
+		border-radius: 0.65rem;
+		margin: 0;
+		outline: 2px dashed #fff;
+		outline-offset: -2px;
+	}
+
+	/* Phone and Tablet (portrait) */
+	@media all and (max-width: 768px) {
+		.container {
+			width: calc(50vw - 1.45rem);
+		}
+	}
+</style>
